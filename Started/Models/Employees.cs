c67faba3ea3ccdb11e31b8e960baf2c533fe266d
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2020-10-05 15:45:36 +0200

    ok
    ok

---
+++ Started/Models/Employees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Started.Models
+{
+    public class Employees
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ Started/Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Started.Models;
+
+namespace Started.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Started.Models.TodoItem> TodoItems { get; set; }
+
+        public DbSet<Started.Models.Employees> Employees { get; set; }
+    }
+}
